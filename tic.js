d73b15face561688bf0a80f1d33da89a7c922542
tahbristol <tahbristol@gmail.com>
2017-01-01 19:41:51 -0500

    Buttons and fadeIn board

--- tic.js
+++ tic.js
@@ -1,6 +1,27 @@
 $(function(){
+    //hide the x and o for each game space.
     $('.X').addClass('hidden');
     $('.O').addClass('hidden');
+
+    //Fade in the game board.
+    $('#start').on('click',function(){
+     $('table').fadeTo('slow',1);
+    });
+
+    var player1;
+    var player2;
+    var comupter;
+    //Set active player
+    $('#play1').on('click',function(){
+        $(this).css('background-color','green');
+        $('#play2').css('background-color','#FCFCFC');
+    });
+    $('#play2').on('click',function(){
+        $(this).css('background-color', 'green');
+        $('#play1').css('background-color','#FCFCFC');
+    });
+
+
 
 
 
