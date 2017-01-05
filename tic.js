40c60b45d35feea4090f7d041e6ae45efcfa1585
tahbristol <tahbristol@gmail.com>
2017-01-05 00:57:00 -0500

    reset button, two player game play, win alert

--- tic.js
+++ tic.js
@@ -1,36 +1,31 @@
 $(function(){
-    //hide the x and o for each game space.
-    $('.X').addClass('hidden');
-    $('.O').addClass('hidden');
-    $('.blank').addClass('hidden');
-    //Fade in the game board.
-
-
+    //Variables
     var player1;
     var player2;
     var comupter;
     var playerNum = 'no';
     var playerNum = 'no';
-    console.log("p1 " +player1);
-    console.log("p2 " +player2);
+    var play1 = ['Player 1'];
+    var play2 = ['Player 2'];
+    var win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],
+              [3,5,7]];
+
+
     //Set active player
     $('#play1').on('click',function(){
         $(this).css('background-color','green');
         $('#play2').css('background-color','#E7E7E7');
-        playerNum='one'
-        player1=0;
-        player2=1;
-        console.log(player1);
-        console.log(player2);
+        playerNum='one';
+        player1=1;
+        player2=0;
     });
     $('#play2').on('click',function(){
         $(this).css('background-color', 'green');
         $('#play1').css('background-color','#E7E7E7');
         playerNum='two';
-        player1=1;
-        player2=0;
-        console.log(player1);
-        console.log(player2);
+        player1=0;
+        player2=1;
+
     });
 
     //Game play
@@ -38,99 +33,132 @@ $(function(){
         if(playerNum !== 'no'){
             $('table').fadeTo('slow',1);
             $('td').text(undefined);
-            console.log("text "+$('#upLeft').text());
-            console.log("text value: "+$('#upLeft').val());
         }
+    });
+
+    $('#reset').on('click',function(){
+        $('.filled').remove();
+        $('.winner').remove();
+         play1 = ['Player 1'];
+         play2 = ['Player 2'];
 
     });
+
+
         $('td').on('click',function(){
 
+            //Add cell position to player array based on grid number.
             if(player1===1){
-                $(this).append('O');
+
+
+            if($(this).attr('id') === 'upLeft'){
+                play1.push(1);
+
+            }else if($(this).attr('id') === 'upMid'){
+                play1.push(2);
+
+            }else if($(this).attr('id') === 'upRight'){
+                play1.push(3);
+
+            }else if($(this).attr('id') === 'midLeft'){
+                play1.push(4);
+
+            }else if($(this).attr('id') === 'midMid'){
+                play1.push(5);
+
+            }else if($(this).attr('id') === 'midRight'){
+                play1.push(6);
+
+            }else if($(this).attr('id') === 'lowLeft'){
+                play1.push(7);
+
+            }else if($(this).attr('id') === 'lowMid'){
+                play1.push(8);
+
+            }else if($(this).attr('id') === 'lowRight'){
+                play1.push(9);
+
+            }
+console.log(play1);
+            //add player piece to gameboard
+            $(this).append("<p  class='filled' >O</p>");
+                //switch players
                 player1=0;
                 player2=1;
-                $(this).addClass('filled');
-                store();
-                win();
 
+                //check for winning numbers in player array
+                look(win,play1);
 
+                //if player 2's turn
             } else if(player2===1){
-                $(this).append('X');
-                player1=1;
-                player2=0;
-                $(this).addClass('filled');
-                store();
-                win();
-            }
 
-        });
+                if($(this).attr('id') === 'upLeft'){
+                    play2.push(1);
 
+                }else if($(this).attr('id') === 'upMid'){
+                    play2.push(2);
 
+                }else if($(this).attr('id') === 'upRight'){
+                    play2.push(3);
 
-    var board = [];
+                }else if($(this).attr('id') === 'midLeft'){
+                    play2.push(4);
 
-    var store = function(){
-        for(var y=0; y<8; y++){
-            board[y]=[];
-        }
-        board[0][0]=$('#upLeft').text();
-        board[0][1]=$('#upMid').text();
-        board[0][2]=$('#upRight').text();
+                }else if($(this).attr('id') === 'midMid'){
+                    play2.push(5);
 
-        board[1][0]=$('#midLeft').text();
-        board[1][1]=$('#midMid').text();
-        board[1][2]=$('#midRight').text();
+                }else if($(this).attr('id') === 'midRight'){
+                    play2.push(6);
 
-        board[2][0]=$('#upLeft').text();
-        board[2][1]=$('#upMid').text();
-        board[2][2]=$('#upRight').text();
+                }else if($(this).attr('id') === 'lowLeft'){
+                    play2.push(7);
 
-        board[3][0]=$('#upLeft').text();
-        board[3][1]=$('#midLeft').text();
-        board[3][2]=$('#lowLeft').text();
+                }else if($(this).attr('id') === 'lowMid'){
+                    play2.push(8);
 
-        board[4][0]=$('#upMid').text();
-        board[4][1]=$('#midMid').text();
-        board[4][2]=$('#lowMid').text();
-
-        board[5][0]=$('#upRight').text();
-        board[5][1]=$('#midRight').text();
-        board[5][2]=$('#lowRight').text();
-
-        board[6][0]=$('#upLeft').text();
-        board[6][1]=$('#midMid').text();
-        board[6][2]=$('#lowRight').text();
-
-        board[7][0]=$('#upRight').text();
-        board[7][1]=$('#midMid').text();
-        board[7][2]=$('#lowLeft').text();
-    };
-
-        var win = function(){
-        for(var i =0; i<board.length;i++){
-
-            console.log("boardLength: " +board.length)
-                var space1=board[i][0];
-                var space2=board[i][1];
-                var space3=board[i][2];
-                console.log("s1"+ space1);
-                console.log("s2"+ space2);
-                console.log("s3"+ space3);
-                if(space1 === "X" || space1 === "O" || space2 === "X" || space2 === "O"   || space3 === "X" || space3 === "O" ){
-                    if(space1 === space2 && space2 === space3 && space1 === space3){
-
-                        $('table').hide();
-                        //$('.gameover').append('<p>GameOver</p>');
-
-                    }
+                }else if($(this).attr('id') === 'lowRight'){
+                    play2.push(9);
 
                 }
 
+                $(this).append("<p  class='filled' style='margin:auto;'>X</p>");
+                player1=1;
+                player2=0;
+
+                look(win,play2);
+            }
+
+        });
+        //Fxn to check if player arrays have winning numbers. Checks after every turn.
+        var look = function(win,p1){
+            console.log(win + "p1" + p1);
+            for(var i=1; i<win.length;i++){
+	               var tempArr = [];
+	                  for(var x=0; x<win[i].length; x++){
+  	                       tempArr[x] = (p1.indexOf(win[i][x]));
+                       }
+                       //console.log(tempArr);
+        if(tempArr[0] !== -1 && tempArr[1] !== -1 && tempArr[2] !== -1){
+console.log(p1[0]+ "Wins!");
+
+            if(p1[0] === 'Player 1'){
+
+                    $('.gameover').append("<h1 class='winner' >player 1 Wins!</h1>");
+            }else if(p1[0] === 'Player 2'){
+                    $('.gameover').append("<h1 class='winner'>player 2 Wins!</h1>");
+            }
+
+
         }
+    }
+};
 
 
 
-    };
+
+
+
+
 
 
 
